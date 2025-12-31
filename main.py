e42a3493f926e405481d3497d390cf87cbed6d16
Кирилл <anonimus228@atomicmail.io>
2025-12-31 14:30:23 +0300

    ОБНОВИЛ ФАЙЛ.
    дописал код

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 import telebot
+import config
 from telebot import types
+bot=telebot.TeleBot(token=config.token)
+@bot.message_handler(commands=['start'])
+def wellcome(m):
+   bot.send_message('Привет я бот')
+
+bot.polling()
