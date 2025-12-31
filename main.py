2ce848952a5f21dd836b4fca93f03940eb1917bc
Кирилл <anonimus228@atomicmail.io>
2025-12-31 14:59:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ bot=telebot.TeleBot(token=config.token)
 # обработка кнопки старт
 @bot.message_handler(commands=['start'])
 def wellcome(m):
-# ответ на сообщение 
+   # ответ на сообщение 
    bot.send_message('Привет я бот')
 
 # Работа по модели long-polling.
